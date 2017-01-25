9c2244a43f101c9a07795e89d13c96337503246e
yashodhar <cs11b022@iith.ac.in>
2017-01-25 23:54:51 +0530

    modified LargestContiguos Sum Array

--- largestSUmContiguosArray.py
+++ largestSUmContiguosArray.py
@@ -1,14 +1,14 @@
-arr=[-2, -3, 4, -1, -2, 1, 5, -3]
+arr=[1, 3, -4, 1, 2, 1, 5, -3]
 sum1=arr[0]
 sum2 = sum1
 n=arr.__len__()
 i=1
 while i<n:
     print sum1,sum2
-    if arr[i]<0 and sum1+arr[i]<0:
+    if sum1<0:
         sum1=arr[i]
     else:
-        sum1= arr[i]
+        sum1+= arr[i]
     if sum2<sum1:
         sum2 = sum1
     i+=1
